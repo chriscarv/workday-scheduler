c792e78521ff9a18cd4cfe72ead3ed64f963764d
christian <christiancarvajal@Christians-MacBook-Air.local>
2022-05-01 20:57:52 -0400

    tasks saved to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,12 @@
 var showDay = document.getElementById("currentDay");
-var date;
-date = moment().format("MMM Do YY"); 
+var date = moment().format("MMM Do YY"); 
+//console.log(date)
+var currentHour = moment().format("H");
+//console.log(currentHour);
 showDay.textContent = date;
+$(".saveBtn").on("click",function(){
+    //console.log("click");
+    var task = $(this).siblings(".description").val().trim();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time,task);
+})
